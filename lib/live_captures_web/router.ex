ca97f40c4eb7e332ba604db4482e7b7e6743ea0b
Boris Kuznetsov <me@achempion.com>
2026-01-16 15:43:31 +0100

    Setup phoenix_live_dashboard component examples

--- lib/live_captures_web/router.ex
+++ lib/live_captures_web/router.ex
@@ -3,6 +3,9 @@ defmodule LiveCapturesWeb.Router do
 
   import LiveCapture.Router
 
+  @live_dashboard_prefix "/phoenix_live_dashboard"
+  def __live_dashboard_prefix__, do: @live_dashboard_prefix
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -25,15 +28,16 @@ defmodule LiveCapturesWeb.Router do
   scope "/" do
     pipe_through :browser
 
+    get "/phoenix_live_dashboard/css-:md5", Phoenix.LiveDashboard.Assets, :css,
+      as: :live_dashboard_asset
+
+    get "/phoenix_live_dashboard/js-:md5", Phoenix.LiveDashboard.Assets, :js,
+      as: :live_dashboard_asset
+
     live_capture "/phoenix_live_dashboard", Phoenix.LiveDashboard.LiveCapture,
       csp_nonce_assign_key: %{
         style: :style_csp_nonce,
         script: :script_csp_nonce
       }
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", LiveCapturesWeb do
-  #   pipe_through :api
-  # end
 end
