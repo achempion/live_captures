ff6be4b3ffa7f2f9318042918841b2678ec52f7a
Boris Kuznetsov <me@achempion.com>
2026-01-16 17:18:52 +0100

    Setup meta tags

--- lib/live_captures_web/controllers/page_controller.ex
+++ lib/live_captures_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule LiveCapturesWeb.PageController do
   use LiveCapturesWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home,
+      page_title: "LiveCaptures - LiveView capture examples",
+      page_description:
+        "Explore LiveCapture demos for Phoenix LiveView components and see how visual capture improves UI quality."
+    )
   end
 end
